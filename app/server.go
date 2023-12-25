7bffe0c8de50469b079998d33d9c52872fc2b668
Vu Duc Noi <noivuduc@gmail.com>
2023-12-25 17:20:51 +0800

    pass 3nd stage

--- app/server.go
+++ app/server.go
@@ -22,7 +22,7 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	_, err = conn.Write([]byte("+PONG\\r\\n"))
+	_, err = conn.Write([]byte("+PONG\r\n"))
 	if err != nil {
 		return
 	}
