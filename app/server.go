9aaa771331adf6b82304cd3973ba3f6fefae1b01
Vu Duc Noi <noivuduc@gmail.com>
2023-12-25 18:04:53 +0800

    pass 4nd stage

--- app/server.go
+++ app/server.go
@@ -19,12 +19,14 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
 	}
-	handleConnection(conn)
 
 }
 
@@ -36,11 +38,9 @@ func handleConnection(conn net.Conn) {
 			return
 		}
 		data = strings.TrimSpace(data)
-		if strings.ToLower(data) == "ping" {
-			_, err = conn.Write([]byte("+PONG\r\n"))
-			if err != nil {
-				return
-			}
+		_, err = conn.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			return
 		}
 	}
 }
